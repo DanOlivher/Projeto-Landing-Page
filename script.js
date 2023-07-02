ae0ce5fc1aa7ed1cb3f827865834d70b00e4f3d4
DanOlivher <oliveiradannisio@gmail.com>
2023-07-02 12:46:57 -0300

    novas imagens, index, script e style

---
+++ script.js
@@ -0,0 +1,65 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var texto = document.getElementById('texto-meio');
+  var titulo = document.getElementById('Titulo-meio');
+  
+  function aparecerTexto() {
+    var alturaJanela = window.innerHeight;
+    var posicaoTexto = texto.getBoundingClientRect().top;
+    var posicaoTitulo = titulo.getBoundingClientRect().top;
+
+    if (posicaoTexto < alturaJanela) {
+      texto.classList.remove('escondido');
+      texto.classList.add('aparecer');
+    }
+
+    if (posicaoTitulo < alturaJanela) {
+      titulo.classList.remove('T-escondido');
+      titulo.classList.add('aparecer');
+    }
+  }
+
+  window.addEventListener('scroll', aparecerTexto);
+});
+
+
+  function aparecerImagem() {
+    var imagem = document.getElementById('imagem');
+    var alturaJanela = window.innerHeight;
+    var posicaoImagem = imagem.getBoundingClientRect().top;
+
+    if (posicaoImagem < alturaJanela) {
+        imagem.style.opacity = 1;
+    }
+}
+
+window.addEventListener('scroll', aparecerImagem);
+
+window.addEventListener('resize', adaptarImagem);
+
+function adaptarImagem() {
+  var imagem = document.getElementById('imagem'); // substitua 'minha-imagem' pelo ID da sua imagem
+  var larguraJanela = window.innerWidth;
+  var novaLargura = larguraJanela * 1.0; // ajuste o valor 0.8 conforme necessário
+
+  imagem.style.width = novaLargura + 'px';
+}
+
+function adaptarTexto() {
+  var infoParagrafos = document.querySelectorAll('#menuinformacional .info p');
+
+  infoParagrafos.forEach(function(paragrafo) {
+    var larguraJanela = window.innerWidth;
+
+    if (larguraJanela < 600) {
+      paragrafo.style.fontSize = '4vw'; // Tamanho do texto proporcional à largura da janela
+    } else if (larguraJanela < 400) {
+      paragrafo.style.fontSize = '3em'; // Tamanho do texto relativo ao elemento pai
+    } else {
+      paragrafo.style.fontSize = '16px'; // Tamanho padrão do texto
+    }
+  });
+}
+
+window.addEventListener('resize', adaptarTexto);
+adaptarTexto();
+
